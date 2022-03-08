ad521db55786c02db68697fa4c82d17c11048791
Johny Lopes <jlopes@metatron.com.br>
2022-03-08 19:41:10 -0300

    feat: add UserConfiguration

---
+++ src/Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Actions.Core.Domain.Users.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Actions.Infrasctructure.Data.Configurations
+{
+    [ExcludeFromCodeCoverage]
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        void IEntityTypeConfiguration<User>.Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToView("User");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name);
+
+            builder.Property(x => x.Email);
+
+            builder.Property(x => x.Password);
+
+            builder.Property(x => x.ProfileId);
+
+            builder.Property(x => x.SecondaryProfileId);
+        }
+    }
+}
