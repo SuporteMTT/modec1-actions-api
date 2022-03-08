09dd589d2e8baeadbb438c1edbe54badba6f86cd
Johny Lopes <jlopes@metatron.com.br>
2022-03-08 19:42:07 -0300

    feat: add ActionsContext

---
+++ src/Infrastructure/Data/ActionsContext.cs
@@ -0,0 +1,30 @@
+﻿using Actions.Core.Domain.Users.Entities;
+using Actions.Infrasctructure.Data.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actions.Infrasctructure.Data
+{
+    public class ActionsContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ActionsContext(DbContextOptions<ActionsContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging();
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
