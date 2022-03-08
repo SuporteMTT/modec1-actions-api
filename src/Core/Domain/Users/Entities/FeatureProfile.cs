83b982ef246e8d9dbaf12df8191d3090059ae1d4
Johny Lopes <jlopes@metatron.com.br>
2022-03-08 19:15:41 -0300

    feat: add featureProfile class

---
+++ src/Core/Domain/Users/Entities/FeatureProfile.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Core.Domain.Impl.Entity;
+using Shared.Core.Domain.Impl.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actions.Core.Domain.Users.Entities
+{
+    public class FeatureProfile : Entity<FeatureProfile, string>
+    {
+        public FeatureEnum Name { get; set; }
+        public string ProfileId { get; set; }
+        public int Actions { get; set; }
+    }
+}
