8ef8b52b65f01c1381e47e2a1cb624721b419ba4
Johny Lopes <jlopes@metatron.com.br>
2022-03-08 19:40:20 -0300

    feat: add user entity

---
+++ src/Core/Domain/Users/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using Shared.Core.Domain.Impl.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actions.Core.Domain.Users.Entities
+{
+    public class User : Entity<User, string>
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ProfileId { get; set; }
+        public string SecondaryProfileId { get; set; }
+        public byte[] Photo { get; set; }
+        public bool? HasPhoto { get; set; }
+    }
+}
