3b2663d4ab78b46dc0de4f618294f216dd079bd1
Johny Lopes <jlopes@metatron.com.br>
2022-03-08 19:16:47 -0300

    chore: add token util

---
+++ src/Core/Domain/Shared/Entities/TokenUtil.cs
@@ -0,0 +1,48 @@
+﻿using Actions.Core.Domain.Shared.Interfaces.Entities;
+using Actions.Core.Domain.Users.Entities;
+using Shared.Core.Domain.Impl.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actions.Core.Domain.Shared.Entities
+{
+    public class TokenUtil : ITokenUtil
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string IpAddress { get; private set; }
+        public string Token { get; private set; }
+        public ICollection<FeatureProfile> Permissions { get; private set; }
+
+        public void FillData(string id,
+            string name,
+            string ipAddress,
+            string token,
+            ICollection<FeatureProfile> permissions)
+        {
+            Id = id;
+            Name = name;
+            IpAddress = ipAddress;
+            Token = token;
+            Permissions = permissions;
+        }
+
+        public bool HasPermission(FeatureEnum feature, ActionEnum action)
+        {
+            var actions = Permissions.SingleOrDefault(x => x.Name == feature)?.Actions;
+
+            return (actions & (int)action) == (int)action;
+        }
+
+        public bool TryGetPermissionAndThrow(FeatureEnum feature, ActionEnum action)
+        {
+            if (!HasPermission(feature, action))
+                throw new UnauthorizedAccessException("You are not authorized to perform this operation");
+
+            return true;
+        }
+    }
+}
---
+++ src/Core/Domain/Shared/Interfaces/Entities/ITokenUtil.cs
@@ -0,0 +1,41 @@
+﻿using Actions.Core.Domain.Users.Entities;
+using Shared.Core.Domain.Impl.Enum;
+using System.Collections.Generic;
+
+namespace Actions.Core.Domain.Shared.Interfaces.Entities
+{
+    public interface ITokenUtil
+    {
+        string Id { get; }
+        string Name { get; }
+        string IpAddress { get; }
+        string Token { get; }
+        ICollection<FeatureProfile> Permissions { get; }
+
+        /// <summary>
+        /// Preenche os dados do token
+        /// </summary>
+        /// <param name="id">Id único</param>
+        /// <param name="name">Nome do usuário</param>
+        /// <param name="ipAddress">IP do usuário</param>
+        /// <param name="token">Token</param>
+        /// <param name="permissions">Permissões <see cref="FeatureProfile"/></param>
+        void FillData(string id, string name, string ipAddress, string token, ICollection<FeatureProfile> permissions);
+
+        /// <summary>
+        /// Verifica se possui permissão a funcionalidade e a ação
+        /// </summary>
+        /// <param name="feature"><see cref="FeatureEnum"/></param>
+        /// <param name="action"><see cref="ActionEnum"/></param>
+        /// <returns></returns>
+        bool HasPermission(FeatureEnum feature, ActionEnum action);
+
+        /// <summary>
+        /// Verifica a permissão à funcionalidade e a ção
+        /// </summary>
+        /// <param name="feature"><see cref="FeatureEnum"/></param>
+        /// <param name="action"><see cref="ActionEnum"/></param>
+        /// <returns>true se possuri a permissão ou <see cref="System.UnauthorizedAccessException"/></returns>
+        bool TryGetPermissionAndThrow(FeatureEnum feature, ActionEnum action);
+    }
+}
