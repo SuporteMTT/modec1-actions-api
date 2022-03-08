249c83d510a8a4b8faddbc78e7fcdc6ad77d733f
Johny Lopes <jlopes@metatron.com.br>
2022-03-08 13:03:57 -0300

    chore: initial commit

---
+++ src/Action/Api/Controllers/ActionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actions.Api.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiExplorerSettings(GroupName = "Action")]
+    [Route("api/v{version:apiVersion}/action")]    
+    public class ActionController : ControllerBase
+    {
+    }
+}
