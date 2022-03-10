b060eba1622e6868c9c4adb2d218c2374f2ec963
Johny Lopes <jlopes@metatron.com.br>
2022-03-10 14:30:24 -0300

    feat: add ActionsStatusCountDto and StatusCountDto

---
+++ src/Core/Domain/Shared/Dtos/ActionsStatusCountDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actions.Core.Domain.Shared.Dtos
+{
+    public class ActionsStatusCountDto
+    {
+        public StatusCountDto NotInitiated { get; set; }
+        public StatusCountDto OnGoing { get; set; }
+        public StatusCountDto ConcludedOrDelayed { get; set; }
+    }
+}
---
+++ src/Core/Domain/Shared/Dtos/StatusCountDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actions.Core.Domain.Shared.Dtos
+{
+    public class StatusCountDto
+    {
+        public int Quantity { get; set; }
+        public string Text { get; set; }
+    }
+}
