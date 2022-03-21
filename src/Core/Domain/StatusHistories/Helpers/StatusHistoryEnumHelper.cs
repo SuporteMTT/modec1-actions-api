f12aa787af3a9d72459065e1fd8e3f5159f32a77
Johny Lopes <jlopes@metatron.com.br>
2022-03-21 14:16:59 -0300

    feat: add StatusHistoryEnumHelper

---
+++ src/Core/Domain/StatusHistories/Helpers/StatusHistoryEnumHelper.cs
@@ -0,0 +1,45 @@
+
+using Actions.Core.Domain.Actions.Enums;
+using Actions.Core.Domain.Shared.Enums;
+using Actions.Core.Domain.StatusHistories.Enums;
+
+namespace Actions.Core.Domain.StatusHistories.Helpers
+{
+    public class StatusHistoryEnumHelper
+    {
+
+        public static StatusHistoryEnum ToStatusHistoryEnum(ActionStatusEnum status)
+        {
+            switch (status)
+            {
+                case ActionStatusEnum.Started:
+                    return StatusHistoryEnum.Started;
+                case ActionStatusEnum.NotInitiated:
+                    return StatusHistoryEnum.NotInitiated;
+                case ActionStatusEnum.Concluded:
+                    return StatusHistoryEnum.Concluded;
+                case ActionStatusEnum.Delayed:
+                    return StatusHistoryEnum.Delayed;
+
+                default:
+                    return StatusHistoryEnum.NotInitiated;
+            }
+        }
+
+        public static StatusHistoryEnum ToStatusHistoryEnum(StatusEnum status)
+        {
+            switch (status)
+            {
+                case StatusEnum.Active:
+                    return StatusHistoryEnum.Active;
+                case StatusEnum.Cancelled:
+                    return StatusHistoryEnum.Cancelled;
+                case StatusEnum.Concluded:
+                    return StatusHistoryEnum.Concluded;
+
+                default:
+                    return StatusHistoryEnum.Active;
+            }
+        }
+    }
+}
